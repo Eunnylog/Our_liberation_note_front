75d5c23b964f51cf979466e9747aa78c090359d1
choiyeji <yj970311@naver.com>
2023-06-08 14:43:52 +0900

    Feat navbar js 기능추가

--- js/loader.js
+++ js/loader.js
@@ -1,39 +1,46 @@
-async function injectNavbar() {
-  fetch("/navbar.html").then(response => {
-    return response.text()
+async function injectNavbar(){
+  fetch("../navbar.html").then(response => {
+      return response.text()
   })
-    .then(data => {
-      document.querySelector("header").innerHTML = data;
-    })
-  let navbarHtml = await fetch("/navbar.html")
+      .then(data => {
+          document.querySelector("header").innerHTML = data;
+      })
+  let navbarHtml = await fetch("../navbar.html")
   let data = await navbarHtml.text()
   document.querySelector("header").innerHTML = data;
 
-  const payload = localStorage.getItem("payload");
-  const cookies = document.cookie.split(';');
+  let nav_login = document.getElementById("nav-login")
+  let nav_signup = document.getElementById("nav-signup")
+  let nav_mydiary = document.getElementById("nav-mydiary")
+      nav_mydiary.style.display = "none"
+  const payload = localStorage.getItem("payload")
 
-  let jwtToken;
+  if(payload){
+      nav_login.style.display = "none"
+      nav_signup.style.display = "none"
+      nav_mydiary.style.display = "block"
 
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].trim();
-    const [name, value] = cookie.split('=');
+      const payload = localStorage.getItem("payload")
+      const payload_parse = JSON.parse(payload)
+      console.log(payload_parse.username)
 
-    if (name === "jwt_token") {
-      jwtToken = value;
-      break;
-    }
+      const intro = document.getElementById("intro")
+
+      intro.innerText = `${payload_parse.username} 님 안녕하세요!`
+
+
+      let navbarRight = document.getElementById("nav-right")
+      let newLi = document.createElement("li")
+      newLi.setAttribute("class", 'nav-link active')
+
+      newLi.innerText = "로그아웃"
+      newLi.setAttribute("onClick", "handleLogout()")
+
+
+
+      navbarRight.appendChild(newLi)
+
+  
   }
-
-  if (jwtToken || payload) {
-    const userdelete = document.getElementById("userdelete")
-    userdelete.innerText = "회원탈퇴"
-    const logout = document.getElementById("logout")
-    logout.innerText = "로그아웃"
-    let loginButton = document.getElementById("login-button")
-    loginButton.style.display = "none";
-    let signupButton = document.getElementById("signup-button")
-    signupButton.style.display = "none";
-  }
-
 }
 injectNavbar();
