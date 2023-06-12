ec53eb6924a5c16e1edaa3f3036417fbd3997f65
Msgun7 <fgarr@naver.com>
2023-06-12 09:46:13 +0900

    Feat: 배포사항공유

--- js/photo_page.js
+++ js/photo_page.js
@@ -1,19 +1,37 @@
-// var swiper = new Swiper(".swiper", {
-//     effect: "coverflow",
-//     grabCursor: true,
-//     centeredSlides: true,
-//     slidesPerView: "auto",
-//     coverflowEffect: {
-//         rotate: 0,
-//         stretch: 0,
-//         depth: 100,
-//         modifier: 2,
-//         slideShadows: true
-//     },
-//     spaceBetween: 60,
-//     loop: true,
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true
-//     }
-// });
+// // 백엔드 연결하기
+// // 사진 추가하기
+// async function addPhoto() {
+//     const img = document.getElementById("img");
+//     const title = documentl.getElementById("title").value;
+//     const location = document.getElementById("location").value;
+//     const status = document.getElementById("status").value;
+//     const memo = document.getElementById("memo").value;
+
+
+//     const formData = new FormData();
+//     formData.append("image", img.files[0]);
+//     formData.append("title", title);
+//     formData.append("location", location);
+//     formData.append("status", status);
+//     formData.append("memo", memo);
+
+
+
+//     response = await fetch(`http://127.0.0.1:8000/photo/`, {
+//         headers: {
+//             // 'Authorization': `Bearer ${accessToken}`
+//         },
+//         method: 'POST',
+//         body: formData
+//     })
+
+//         .then(response => response.json())
+
+//         .then(data => {
+            
+//         })
+
+//         .catch(error => {
+//             alert("에러가 발생했습니다.");
+//         });
+// }
