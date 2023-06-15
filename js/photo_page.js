5029bf0bce462bfc600ff93b82c9671fdf7f09ef
Msgun7 <fgarr@naver.com>
2023-06-15 17:37:35 +0900

    Refactor: 사진첩 상세 페이지와 수정페이지 수정중

--- js/photo_page.js
+++ js/photo_page.js
@@ -9,18 +9,27 @@ const frontend_base_url = "http://127.0.0.1:5500"
 // 사진 추가하기
 async function addPhoto() {
     const image = document.getElementById("image");
+    const name = document.getElementById("name").value;
     const title = document.getElementById("title").value;
     const location = document.getElementById("location").value;
     const memo = document.getElementById("memo").value;
+    let location_x = document.getElementById("location_x").value
+    let location_y = document.getElementById("location_y").value
 
+    // const location_x = document.getElementById("location_x").value
+    // const location_y = document.getElementById("location_y").value
 
     const formData = new FormData();
     formData.append("image", image.files[0]);
+    formData.append("name", name);
     formData.append("title", title);
     formData.append("location", location);
     formData.append("memo", memo);
+    formData.append("location_x", location_x);
+    formData.append("location_y", location_y);
 
     console.log(formData)
+    // console.log(location_x, location_y)
 
     try {
         const urlParams = new URLSearchParams(window.location.search);
@@ -62,13 +71,13 @@ async function album() {
     const response_json = await response.json()
 
     response_json.forEach((a) => {
-        const image = backend_base_url + a["image"];
+        const image = backend_base_url + '/note' + a["image"];
         const title = a['title']
         const location = a['location']
         const memo = a['memo']
         const photo_id = a['id']
         console.log(image)
-
+        console.log(response_json)
         let temp_html = `
             <div class="gallery-item">
             <a class="photo" href="" onclick="photo_detail('${photo_id}')" data-bs-toggle="modal" data-bs-target="#photo-detail"><img class="gallery-image" src="${image}" alt="${title}"></a>
@@ -96,13 +105,11 @@ async function photo_detail(photo_id) {
     // json 형태의 데이터를 가져오고 해당 데이터를 patch로 수정
     console.log(response_json)
 
-    const image = backend_base_url + response_json["image"];
+    const image = backend_base_url + '/note' + response_json["image"];
 
 
     let temp_html = `
                     <img class="gallery-image" src="${image}">
-                    <input id="image" name="files" multiple accept=".jpg, .png, .jpeg" type="file"
-                        class="form-control" placeholder="사진">
                     <input name="title" id="title" type="text" class="form-control" placeholder="제목">
                     <input name="location" id="location" type="text" class="form-control" placeholder="주소">
                     <input id="memo" type="text" class="form-control" placeholder="메모">
@@ -111,7 +118,7 @@ async function photo_detail(photo_id) {
 
 }
 
-function patchBox() {
+function patchPhotoBox() {
     // 수정 창으로 변경합니다.
     let image = document.getElementById("image");
     let title = document.getElementById('photo_title').innerHTML;
@@ -120,33 +127,41 @@ function patchBox() {
 
 
     planInfoDiv.innerHTML = `
-                            <div class="input-group" style="flex-wrap: nowrap;">
-                                <input name="title" id="title" type="text" value='${title}' class="form-control" placeholder="사진 타이틀"
-                                style="width: 60%; height:40px;">
-                                <img class="gallery-image" src="${image}">
-                                <input id="image" name="files" multiple accept=".jpg, .png, .jpeg" type="file"
-                                class="form-control" placeholder="사진">
+                            <div class="filebox">
+                                <input class="upload-name" value="첨부파일" src="${image}" placeholder="첨부파일" multiple
+                                    accept=".jpg, .png, .jpeg" style="width: 85.3%">
+                                <label for="image">사진추가</label>
+                                <input type="file" id="image" style="display: none" />
+                            </div>
+                            <div class="input-group-append" style="width: 100%;">
+                                <input name="name" id="name" type="text" value='${title}' class="form-control"
+                                    placeholder="사진 타이틀" style="width: 100%; height:40px;">
+                            </div>
+                            <div class="input-group-append" style="width: 100%;">
+                                <input name="title" id="title" value='${location}' type="text" class="form-control"
+                                    placeholder="주소 검색" style="width: 100%; height:40px;">
+                            </div>
+                            <div class="input-group"  style="flex-wrap: nowrap;">
+                                <div class="input-group-append" style="width: 100%;">
+                                    <input name="location" id="location" type="text" class="form-control"
+                                    placeholder="장소" style="width: 99%; height:40px;" placeholder="주소(미작성시 AI사용이 불가합니다!)">
+                                </div>
                                 <div class="input-group-append" style="width: 10%;">
                                     <button type="button" onclick="searchLocation()" class="btn btn-primary"
                                     style="margin-top:0px;height:40px; font-size:15px">검색</button>
                                 </div>
-                            </div>
-                            <div id="search_box" style="width: 100%;  overflow: auto;"></div>
-                            <input name="location" id="location" value='${location}' type="text" class="form-control"
-                                placeholder="주소(미작성시 AI사용이 불가합니다!)">
-                            <input name="start" id="start" value='${date}' type="date" class="form-control">
-                            <input name="time" id="time" value='${time}' type="text" class="form-control" placeholder="시간">
                             <textarea name="memo" id="memo" value='${memo}'  type="textarea" class="form-control" placeholder="memo"
                                 style="height:200px; min-height:200px; max-height:200px"></textarea>
     `;
 
-    const btnElement = document.getElementById('patch_box');
+    const btnElement = document.getElementById('patch_photo_box');
     btnElement.innerText = '저장';
     btnElement.setAttribute("onClick", `patchPhoto()`)
 }
 
 async function patchPhoto() {
     photo_id = document.getElementById('photo_modal_id').innerHTML;
+    let name = document.getElementById('name');
     let title = document.getElementById('title');
     let location = document.getElementById('location');
     let memo = document.getElementById('memo');
@@ -157,10 +172,11 @@ async function patchPhoto() {
     const response = await fetch(`${back_url}/note/photo-detail/${photo_id}`, {
         headers: {
             'content-type': 'application/json',
-            "Authorization": `Bearer ${access_token}`,
+            // "Authorization": `Bearer ${access_token}`,
         },
         method: 'PATCH',
         body: JSON.stringify({
+            "name": name.value ?? name.placeholder,
             "title": title.value ?? title.placeholder,
             "location": location.value ?? location.placeholder,
             "memo": memo.value ?? memo.placeholder,
@@ -175,3 +191,10 @@ async function patchPhoto() {
         alert('문제가 발생했습니다!')
     }
 }
+
+//photo_page.html > 사진추가 버튼 옆 업로드 이름 
+$("#image").on('change', function () {
+    var fileName = $("#image").val();
+    $(".upload-name").val(fileName);
+});
+
