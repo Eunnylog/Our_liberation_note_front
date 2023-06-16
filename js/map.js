61980cf0673b912d8d2be80e6838b4976b427e21
Msgun7 <fgarr@naver.com>
2023-06-17 00:22:21 +0900

    Refactor: 사진 상세페이지 장소수정

--- js/map.js
+++ js/map.js
@@ -1,40 +1,46 @@
-async function searchLocation() {
+async function searchLocation(box_id) {
     const REST_API_KEY = api_key.KAKAO;
-    const query = document.getElementById('title').value;
-
+    let query
+    if (box_id == "2") {
+        query = document.getElementById('p_title').value;
+    } else if (box_id == "1") {
+        query = document.getElementById('title').value;
+    }
     const url = `https://dapi.kakao.com/v2/local/search/keyword.json?query=${encodeURIComponent(query)}`;
     const headers = {
         'Authorization': `KakaoAK ${REST_API_KEY}`
     };
+    if (box_id = "2") {
+        axios.get(url, { headers })
+            .then(response => {
+                const places = response.data.documents;
+                var searchBox = document.getElementById('search_box');
+                searchBox.style.height = '200px';
+                searchBox.style.padding = '20px';
+                searchBox.style.margin = '20px auto 20px auto';
+                // 검색 결과 처리
+                console.log("hi!")
+                $('#search_box2').empty()
 
-    axios.get(url, { headers })
-        .then(response => {
-            const places = response.data.documents;
-            var searchBox = document.getElementById('search_box');
-            searchBox.style.height = '200px';
-            searchBox.style.padding = '20px';
-            searchBox.style.margin = '20px auto 20px auto';
-            // 검색 결과 처리
-            $('#search_box').empty();
-            places.forEach((place, index) => {
-                const address_name = place.address_name
-                const place_name = place.place_name
-                const place_url = place.place_url
-                const place_category = place.category_group_name
-                const location_x = place.x
-                const location_y = place.y
+                places.forEach((place, index) => {
+                    const address_name = place.address_name
+                    const place_name = place.place_name
+                    const place_url = place.place_url
+                    const place_category = place.category_group_name
+                    const location_x = place.x
+                    const location_y = place.y
 
-                console.log(location_x, location_y)
+                    console.log(location_x, location_y)
 
 
-                let temp_html = `
+                    let temp_html = `
                                 <div style="display: flex; align-items: center; justify-content: space-between; margin: 20px auto;">
                                     <div>
                                         <h3 style="font-size: 15px; margin-bottom: 5px;" id='name_${index}' >${place_name} / ${place_category}</h3>
                                         <a href="${place_url}" id='address_${index}' target="_blank" style="font-size: 15px; color: black;">${address_name}</a>
                                     </div>
                                     <div style="display: flex; align-items: center;">
-                                    <input type="radio" name="address_radio" value="${index}" style="width: 10px; margin-right: 10px;" onclick="handleRadio()">
+                                    <input type="radio" name="address_radio" value="${index}" style="width: 10px; margin-right: 10px;" onclick="handleRadio(2)">
                                         <label for="${index}" style="font-size: 15px;">선택</label>
                                     <input id='x_${index}' value="${location_x}" hidden>
                                     <input id='y_${index}' value="${location_y}" hidden>
@@ -43,33 +49,97 @@ async function searchLocation() {
                                 <hr>
                                 <br>
                             `
-                $('#search_box').append(temp_html);
+                    $('#search_box2').append(temp_html);
+                });
+            })
+            .catch(error => {
+                // 에러 처리
+                console.error(error);
+                alert('문제가 발생했습니다!')
             });
-        })
-        .catch(error => {
-            // 에러 처리
-            console.error(error);
-            alert('문제가 발생했습니다!')
-        });
 
+    } else {
+        axios.get(url, { headers })
+            .then(response => {
+                const places = response.data.documents;
+                var searchBox = document.getElementById('search_box');
+                searchBox.style.height = '200px';
+                searchBox.style.padding = '20px';
+                searchBox.style.margin = '20px auto 20px auto';
+                // 검색 결과 처리
+                $('#search_box').empty()
+                places.forEach((place, index) => {
+                    const address_name = place.address_name
+                    const place_name = place.place_name
+                    const place_url = place.place_url
+                    const place_category = place.category_group_name
+                    const location_x = place.x
+                    const location_y = place.y
+
+                    console.log(location_x, location_y)
+
+
+                    let temp_html = `
+                                <div style="display: flex; align-items: center; justify-content: space-between; margin: 20px auto;">
+                                    <div>
+                                        <h3 style="font-size: 15px; margin-bottom: 5px;" id='name_${index}' >${place_name} / ${place_category}</h3>
+                                        <a href="${place_url}" id='address_${index}' target="_blank" style="font-size: 15px; color: black;">${address_name}</a>
+                                    </div>
+                                    <div style="display: flex; align-items: center;">
+                                    <input type="radio" name="address_radio" value="${index}" style="width: 10px; margin-right: 10px;" onclick="handleRadio(1)">
+                                        <label for="${index}" style="font-size: 15px;">선택</label>
+                                    <input id='x_${index}' value="${location_x}" hidden>
+                                    <input id='y_${index}' value="${location_y}" hidden>
+                                    </div>
+                                </div>
+                                <hr>
+                                <br>
+                            `
+                    console.log("hi!")
+                    $('#search_box').append(temp_html);
+                });
+            })
+            .catch(error => {
+                // 에러 처리
+                console.error(error);
+                alert('문제가 발생했습니다!')
+            });
+
+    }
 }
 
-function handleRadio() {
+
+
+function handleRadio(box_id = 0) {
     var selectedRadio = document.querySelector('input[name="address_radio"]:checked');
-    if (selectedRadio) {
-        let selectedIndex = selectedRadio.value;
-        let selected_address = document.getElementById(`address_${selectedIndex}`).innerText;
-        let selected_name = document.getElementById(`name_${selectedIndex}`).innerText;
-        let location_x = document.getElementById(`x_${selectedIndex}`).value;
-        let location_y = document.getElementById(`y_${selectedIndex}`).value;
-        let name = selected_name.split('/')[0].trim();
-        let splitName = selected_name.split('/');
+    let selectedIndex = selectedRadio.value;
+    let selected_address = document.getElementById(`address_${selectedIndex}`).innerText;
+    let selected_name = document.getElementById(`name_${selectedIndex}`).innerText;
+    let location_x = document.getElementById(`x_${selectedIndex}`).value;
+    let location_y = document.getElementById(`y_${selectedIndex}`).value;
+    let name = selected_name.split('/')[0].trim();
+    let splitName = selected_name.split('/');
+
+    if (box_id == 0) {
         let category = splitName[splitName.length - 1].trim();
+        document.getElementById("category").value = category;
+        document.getElementById("location").value = selected_address;
+        document.getElementById("title").value = name;
+        document.getElementById("location_x").value = location_x;
+        document.getElementById("location_y").value = location_y;
+    } else if (box_id == 1) {
         // 선택한 요소에 대한 처리
         document.getElementById("location").value = selected_address;
         document.getElementById("title").value = name;
         document.getElementById("location_x").value = location_x;
         document.getElementById("location_y").value = location_y;
-        document.getElementById("category").value = category;
+    } else if (box_id == 2) {
+        // 선택한 요소에 대한 처리
+        document.getElementById("p_location").value = selected_address;
+        document.getElementById("p_title").value = name;
+        document.getElementById("p_location_x").value = location_x;
+        document.getElementById("p_location_y").value = location_y;
     }
+
+
 }
--- js/photo_page.js
+++ js/photo_page.js
@@ -110,7 +110,7 @@ async function photo_detail(photo_id) {
     const title = response_json["title"]
     const location = response_json["location"]
     const memo = response_json["memo"]
-
+    // const photo_id = response_json["photo_id"]
 
 
     let temp_html = `
@@ -120,17 +120,23 @@ async function photo_detail(photo_id) {
                     <div id='photo_title'>${title}</div> 
                     <div id='photo_location'>${location}</div>
                     <div id='photo_memo'>${memo}</div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">취소하기
+                        </button>
+                        <button id="patch_photo_box" type="button" class="btn btn-primary"
+                            onclick="patchPhotoBox('${photo_id}')">수정</button>
+                    </div>
                     `
     $('#photo-d').append(temp_html)
 
 }
 
 
-function patchPhotoBox(response_json) {
-
+function patchPhotoBox(photo_id) {
+    console.log(photo_id)
     // // 수정 창으로 변경합니다.
     // let photo_detail = document.getElementById('photo-d');
-    let image = document.getElementById("photo_image");
+    let image = document.getElementById('photo_image');
     let name = document.getElementById('photo_name').innerHTML;
     let start = document.getElementById('photo_start').innerHTML;
     let title = document.getElementById('photo_title').innerHTML;
@@ -138,7 +144,7 @@ function patchPhotoBox(response_json) {
     let memo = document.getElementById('photo_memo').innerHTML;
 
     $('#photo-d').empty();
-    temp_html = `
+    temp_html = `           
                             <div class="filebox">
                                 <input class="upload-name" value="첨부파일" src="${image}" placeholder="첨부파일" multiple
                                     accept=".jpg, .png, .jpeg" style="width: 85.3%">
@@ -146,46 +152,64 @@ function patchPhotoBox(response_json) {
                                 <input type="file" id="image" style="display: none" />
                             </div>
                             <div class="input-group-append" style="width: 100%;">
-                                <input name="name" id="name" type="text" value='${name}' class="form-control"
+                                <input name="name" id="p_name" type="text" value='${name}' class="form-control"
                                     placeholder="사진 타이틀" style="width: 100%; height:40px;">
-                                <input name="start" id="start" type="date" value='${start}' class="form-control">
+                                <input name="start" id="p_start" type="date" value='${start}' class="form-control">
                             </div>
                             <div class="input-group-append" style="width: 100%;">
-                                <input name="title" id="title" value='${title}' type="text" class="form-control"
+                                <input name="title" id="p_title" value='${title}' type="text" class="form-control"
                                     placeholder="주소 검색" style="width: 100%; height:40px;">
                             </div>
                             <div class="input-group"  style="flex-wrap: nowrap;">
-                                <div class="input-group-append" style="width: 100%;">
-                                    <input name="location" id="location" value='${location}' type="text" class="form-control"
-                                    placeholder="장소" style="width: 99%; height:40px;" placeholder="주소(미작성시 AI사용이 불가합니다!)">
+                                <div class="input-group-append" style="width: 90%;">
+                                    <input name="location" id="p_location" value='${location}' type="text" class="form-control"
+                                    placeholder="장소" style="width: 90%; height:40px;" placeholder="주소(미작성시 AI사용이 불가합니다!)">
                                 </div>
                                 <div class="input-group-append" style="width: 10%;">
-                                    <button type="button" onclick="searchLocation()" class="btn btn-primary"
+                                    <button type="button" onclick="searchLocation(2)" class="btn btn-primary"
                                     style="margin-top:0px;height:40px; font-size:15px">검색</button>
                                 </div>
-                            <textarea name="memo" id="memo" type="textarea" class="form-control" placeholder="memo"
-                                style="height:200px; min-height:200px; max-height:200px">${memo}</textarea>
-    `;
+                            </div>
+                            <div id="search_box2" style="width: 100%;  overflow: auto; height= 30px;"></div>
+                            <div class="input-group" style="flex-wrap: nowrap;">
+                                <textarea name="memo" id="p_memo" type="textarea" class="form-control" placeholder="memo"
+                                style="height:50px; min-height:50px; max-height:200px; width:100%" >${memo}</textarea>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">취소하기
+                                </button>
+                                <button id="patch_photo" value='${photo_id}' type="button" class="btn btn-primary"
+                                    onclick="patchPhoto()">저장</button>
+                            </div> `;
     $('#photo-d').append(temp_html)
 
 
-    const btnElement = document.getElementById('patch_photo_box');
-    btnElement.innerText = '저장';
-    btnElement.setAttribute("onClick", `patchPhoto()`)
+    // const btnElement = document.getElementById('patch_photo_box');
+    // btnElement.innerText = '저장';
+    // btnElement.setAttribute("onClick", `patchPhoto()`)
 }
 
 async function patchPhoto() {
+    const photo_id = document.getElementById("patch_photo").value;
     const image = document.getElementById("image");
-    const name = document.getElementById('name').value;
-    const title = document.getElementById('title').value;
-    const start = document.getElementById('start').value;
-    const location = document.getElementById('location').value;
-    const memo = document.getElementById('memo').value;
+    const name = document.getElementById('p_name').value;
+    const title = document.getElementById('p_title').value;
+    const start = document.getElementById('p_start').value;
+    const location = document.getElementById('p_location').value;
+    const memo = document.getElementById('p_memo').value;
     const location_x = document.getElementById("location_x").value
     const location_y = document.getElementById("location_y").value
 
+
+
     const formData = new FormData();
-    formData.append("image", image.files[0]);
+
+    // 기존 이미지를 삭제하기 위해 'delete_image' 파라미터를 추가하여 서버에 전달
+    formData.append("delete_image", true);
+    // 새로운 이미지가 선택되었을 경우에만 새로운 이미지를 추가합니다.
+    if (image.files.length > 0) {
+        formData.append("image", image.files[0]);
+    }
     formData.append("name", name);
     formData.append("title", title);
     formData.append("start", start);
@@ -194,20 +218,19 @@ async function patchPhoto() {
     formData.append("location_x", location_x);
     formData.append("location_y", location_y);
 
-    if (!image || !name || !title || !location || !memo) {
-        alert("모든 값을 입력해주세요.");
-        return;
+    console.log(name)
+    for (const pair of formData.entries()) {
+        console.log(pair[0] + ':', pair[1]);
     }
     try {
         const urlParams = new URLSearchParams(window.location.search);
-        const note_id = urlParams.get('note_id');
+        // const photo_id = urlParams.get('photo_id');
 
-        const response = await fetch(`${backend_base_url}/note/photo/${note_id}`, {
+        const response = await fetch(`${backend_base_url}/note/photo-detail/${photo_id}`, {
             // headers: {
             //     // "Authorization": `Bearer ${access_token}`,
             // },
             method: 'PATCH',
-
             body: formData
         });
 
@@ -221,6 +244,7 @@ async function patchPhoto() {
     } catch (error) {
         alert("에러가 발생했습니다.");
         console.error(error);
+        // window.location.reload()
     }
 }
 //photo_page.html > 사진추가 버튼 옆 업로드 이름 
