10e2a7f484202b6d8dd9232d27bae8e6a2745eff
miyeong <kmy9810@naver.com>
2023-06-22 15:07:56 +0900

    Fix: 배포url 오류 수정

--- js/api.js
+++ js/api.js
@@ -15,6 +15,7 @@ async function handleSignup() {
   const password2 = document.getElementById("password2").value
   const confirmcode = document.getElementById("confirmcode").value
 
+
   const response = await fetch(`${backend_base_url}/user/signup/`, {
     headers: {
       'content-type': 'application/json',
@@ -81,38 +82,42 @@ async function signupTimer() {
 async function handleSignin() {
   const email = document.getElementById("login-email").value
   const password = document.getElementById("login-password").value
-
-  const response = await fetch(`${backend_base_url}/user/login/`, {
-    headers: {
-      'content-type': 'application/json',
-    },
-    method: 'POST',
-    body: JSON.stringify({
-      "email": email,
-      "password": password,
+  try {
+    const response = await fetch(`${backend_base_url}/user/login/`, {
+      headers: {
+        'content-type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        "email": email,
+        "password": password,
+      })
     })
-  })
 
-  if (response.status == 200) {
-    const response_json = await response.json()
+    if (response.status == 200) {
+      const response_json = await response.json()
 
-    // localstorage에 저장하기
-    localStorage.setItem('refresh', response_json.refresh)
-    localStorage.setItem('access', response_json.access)
-    console.log(response_json)
-    const base64Url = response_json.access.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''))
+      // localstorage에 저장하기
+      localStorage.setItem('refresh', response_json.refresh)
+      localStorage.setItem('access', response_json.access)
+      console.log(response_json)
+      const base64Url = response_json.access.split('.')[1];
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''))
 
-    localStorage.setItem('payload', jsonPayload)
-    document.getElementById("login").querySelector('[data-bs-dismiss="modal"]').click();
-    location.reload()
+      localStorage.setItem('payload', jsonPayload)
+      document.getElementById("login").querySelector('[data-bs-dismiss="modal"]').click();
+      location.reload()
+    }
+    else {
+      alert("※이메일 혹은 비밀번호가 올바르지 않습니다!")
+      console.log(response)
+    }
   }
-  else {
-    alert("※이메일 혹은 비밀번호가 올바르지 않습니다!")
-    console.log(response)
+  catch (error) {
+    console.log(error)
   }
 }
 
