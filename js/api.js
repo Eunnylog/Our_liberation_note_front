4616b8a158a9c625551279e0a17bc3b94fbe17ed
miyeong <kmy9810@naver.com>
2023-06-25 15:23:35 +0900

    Style: 사진첩 필수요소 표시 추가

--- js/api.js
+++ js/api.js
@@ -361,11 +361,10 @@ function checkLogin() {
   const payload = localStorage.getItem("payload");
 
   if (!payload) {
-    showToast('로그인 또는 회원가입이 필요합니다!')
+    alert('로그인 또는 회원가입이 필요합니다!')
     window.location.replace(`${frontend_base_url}/index.html`)
   }
 }
-
 
 // 회원탈퇴
 async function handlesUserDelete() {
