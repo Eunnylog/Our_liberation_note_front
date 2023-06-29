23bc415b3df429a7fce08c63be1b567d6459a779
Msgun7 <fgarr@naver.com>
2023-06-29 21:25:51 +0900

    Fix: 이미지 용량증설

--- js/photo_page.js
+++ js/photo_page.js
@@ -31,8 +31,8 @@ async function addPhoto() {
     formData.append("location_x", location_x);
     formData.append("location_y", location_y);
 
-    if (image.files[0].size > 1 * 1024 * 1024) {
-        alert("첨부파일 사이즈는 1MB 이내로 등록이 가능합니다.");
+    if (image.files[0].size > 10 * 1024 * 1024) {
+        alert("첨부파일 사이즈는 10MB 이내로 등록이 가능합니다.");
         return false;
     }
 
