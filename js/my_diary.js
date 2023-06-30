2f3caf81202d3ee55ee5ea00339cd2a7d09d9a62
miyeong <kmy9810@naver.com>
2023-06-30 17:27:06 +0900

    Fix: 노트삭제 오류 수정

--- js/my_diary.js
+++ js/my_diary.js
@@ -154,7 +154,7 @@ async function showNoteList() {
             const name = a['name']
             const note_id = a['id']
             let temp_html = `
-                                <a href='#' style='text-decoration:none; color:black;'>
+                                <a href='/plan_page.html?note_id=${note_id}' style='text-decoration:none; color:black;'>
                                     <section class="cp-card content" href="/plan_page.html?note_id=${note_id}" onclick="event.preventDefault(); saveLocalNoteName('${name}','${group_id}', '${note_id}'); showMoveNoteModal();" style="background-image: url('/css/note_img/note_${category}.png');">
                                         <div class="thumb"></div>
                                     </section>
